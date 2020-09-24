a47258346aa121c9a1fee7e254f1933d21ed0386
SergyNovikLV <mr.noviks@gmail.com>
2020-09-24 11:07:40 +0300

    Initial project

---
+++ src/components/TodoForm.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { useState } from "react"
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
+
+function TodoForm({ addTodo }) {
+    const [todo, setTodo] = useState({
+        id: "",
+        task: "",
+        completed: false
+    });
+
+    function handleTaskInputChange(e) {
+        setTodo({ ...todo, task: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (todo.task.trim()) {
+            addTodo({ ...todo, id: uuidv4() });
+
+            setTodo({ ...todo, task: "" });
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                name="task"
+                type="text"
+                value={"todo.task"}
+                onChange={handleTaskInputChange}
+            />
+            <button type="submit">submit</button>
+        </form>
+     
+    );
+}
+
+export default TodoForm;
